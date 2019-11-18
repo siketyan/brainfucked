73df14debbf7787c0ef7e9bbe9252d379424dbbd
Naoki Ikeguchi <siketyan@sikeserver.com>
2019-11-18 13:08:00 +0900

    Add Transaction

---
+++ tests/Reader/TransactionTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Siketyan\Brainfucked\Reader;
+
+use PHPUnit\Framework\TestCase;
+use Prophecy\Prophecy\ObjectProphecy;
+
+class TransactionTest extends TestCase
+{
+    /**
+     * @var ObjectProphecy|ReaderInterface the reader to test with
+     */
+    private $readerP;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp(): void
+    {
+        $this->readerP = $this->prophesize(ReaderInterface::class);
+    }
+
+    /**
+     * Tests the transaction.
+     */
+    public function test(): void
+    {
+        $this->readerP
+            ->getPosition()
+            ->willReturn(123)
+            ->shouldBeCalledOnce();
+
+        $this->readerP
+            ->seek(123)
+            ->shouldBeCalledOnce();
+
+        $transaction = new Transaction(
+            $this->readerP->reveal()
+        );
+
+        $transaction->rollback();
+    }
+}
