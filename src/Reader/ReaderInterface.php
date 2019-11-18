cbd39658aba33852ebe8fc4bd71dc099175a496e
Naoki Ikeguchi <siketyan@sikeserver.com>
2019-11-18 12:59:56 +0900

    Add ReaderInterface

---
+++ src/Reader/ReaderInterface.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Siketyan\Brainfucked\Reader;
+
+interface ReaderInterface
+{
+    /**
+     * Reads a character.
+     *
+     * @return string the read character
+     */
+    public function read(): string;
+
+    /**
+     * Seeks the reader to the position.
+     *
+     * @param int $position the position to seek to
+     */
+    public function seek(int $position): void;
+
+    /**
+     * Gets the position of the reader.
+     *
+     * @return int the current position
+     */
+    public function getPosition(): int;
+
+    /**
+     * Checks whether the reader can read more or not.
+     *
+     * @return bool true if the reader is available to read
+     */
+    public function isAvailable(): bool;
+}
