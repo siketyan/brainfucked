4e177d3ac60e6d78c893242212c2292b42f0d212
Naoki Ikeguchi <siketyan@sikeserver.com>
2019-12-23 00:34:48 +0900

    Add Decrement

---
+++ src/Instruction/Decrement.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Siketyan\Brainfucked\Instruction;
+
+use Siketyan\Brainfucked\Language\LanguageInterface;
+use Siketyan\Brainfucked\Memory\MemoryInterface;
+use Siketyan\Brainfucked\Runner\RunnerInterface;
+
+class Decrement implements InstructionInterface
+{
+    /**
+     * @var MemoryInterface the memory to operate
+     */
+    private $memory;
+
+    /**
+     * Decrement constructor.
+     *
+     * @param MemoryInterface $memory the memory to operate
+     */
+    public function __construct(MemoryInterface $memory)
+    {
+        $this->memory = $memory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __toString(): string
+    {
+        return '-';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function do(RunnerInterface $runner): void
+    {
+        $this->memory->set($this->memory->get() - 1);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getToken(LanguageInterface $language): string
+    {
+        return $language->decrement();
+    }
+}
