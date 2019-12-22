3e02216d995ff3cca2cc7dd1387f75be80a0dde9
Naoki Ikeguchi <siketyan@sikeserver.com>
2019-12-22 20:17:34 +0900

    Add Sourcemap

---
+++ tests/Runtime/SourcemapTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Siketyan\Brainfucked\Runtime;
+
+use PHPUnit\Framework\TestCase;
+
+class SourcemapTest extends TestCase
+{
+    private const SOURCE = 'protocol://dummy.bf';
+    private const POSITION = '1234';
+
+    /**
+     * @var Sourcemap the sourcemap to test
+     */
+    private $sourcemap;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp(): void
+    {
+        $this->sourcemap = new Sourcemap(
+            self::SOURCE,
+            self::POSITION
+        );
+    }
+
+    /**
+     * Tests the sourcemap.
+     */
+    public function test(): void
+    {
+        $this->assertInstanceOf(Sourcemap::class, $this->sourcemap);
+        $this->assertSame(self::SOURCE, $this->sourcemap->getSource());
+        $this->assertSame(self::POSITION, $this->sourcemap->getPosition());
+    }
+}
